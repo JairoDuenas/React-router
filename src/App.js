a2f95119ddf8559904bc4305623a54e51ab71f2d
JairoDuenas <jairoduenas.ing@gmail.com>
2022-10-06 09:56:57 -0500

    Componentes de navegación

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import BlogPage from './pages/BlogPage';
+import HomePage from './pages/HomePage';
+import Menu from './pages/Menu';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HashRouter>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/blog' element={<BlogPage />} />
+          <Route path='/profile' element={<ProfilePage />} />
+          <Route path='*' element={<h2>Not Found</h2>} />
+        </Routes>
+      </HashRouter>
+    </>
   );
 }
 
---
+++ src/pages/Menu/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <div>
+      <nav>Menu</nav>
+    </div>
+  );
+}
+
+export default Menu;

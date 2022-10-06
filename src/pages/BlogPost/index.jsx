d14da3c0f787a92697bab64c294a0311dc8b46a2
JairoDuenas <jairoduenas.ing@gmail.com>
2022-10-06 11:28:13 -0500

    useNavigate: historial de navegación

--- src/pages/BlogPost/index.jsx
+++ src/pages/BlogPost/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import blogData from '../../data/BlogData';
 
 const BlogPost = () => {
+
+  const navigate = useNavigate();
+
+  const returnToBlog = () => {
+    navigate('/blog'); // navigate(-1) 'solo va hacia atrás'
+  }
+
   const { slug } = useParams();
   const blogPost = blogData.find(post => post.slug === slug)
 
   return (
     <>
       <h2>{blogPost.title}</h2>
+      <button onClick={returnToBlog} >Volver al Blog</button>
       <p>{blogPost.author}</p>
       <p>{blogPost.content}</p>
     </>
